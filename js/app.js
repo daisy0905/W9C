0ac9ee3dccdf6e125351a609502c5ba3a1143710
daisy0905 <dtong0905@gmail.com>
2020-08-20 20:49:09 -0600

    Created array of Tweets

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var tweetTopic = ["Alberta chief medical officer to update COVID-19 reponse Thursday", "Calgary brewery resurrects more than century-old beverage Cronk", "Popular Calgary obstacle course center InjaNation set to close", "There is no longer a heat warning in place for Calgary", "Calgary couple arrested, drugs and guns seized", "Massive smoke plumes rising from wildfire burining south of Penticton", "Trudeau planning social welfare system overhaul", "Fire investigator probes cause of northwest Calgary house fire", "Solar project seeks to replace abandoned oil wells in rural Alberta", "What to know about the CRA breach and shutdown"];
+var arrayLength = tweetTopic.length;
+
+for(var counter = 0; counter < tweetTopic.length; counter++) {
+    console.log(tweetTopic[counter]);
+}
